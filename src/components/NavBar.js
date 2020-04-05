842372399c457c2fb1f4b6ac237cfaf214fc52da
jtobin321 <jtobin321@gmail.com>
2020-04-04 21:36:27 -0400

    made updates for the body

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,19 +14,18 @@ const NavBar = (props) => {
         <div>
             <Menu fixed='top' inverted>
                 <Container>
-                    <Menu.Item as='a' header>
+                    <Menu.Item disabled header>
                         <Image size='mini' src={logo} style={{ marginRight: '1.5em' }} />
-          Jack Tobin
-        </Menu.Item>
-                    <Menu.Item as='a'>Home</Menu.Item>
+                        <p style={{ color: "white" }}>Jack Tobin</p>
+                    </Menu.Item>
                     <Menu.Item as='a'>About Me</Menu.Item>
                     <Menu.Item as='a'>Projects</Menu.Item>
-
                     <Dropdown item simple text='Contact'>
                         <Dropdown.Menu>
                         <Dropdown.Item href="mailto:jtobin321@gmail.com" target="_blank">Email - jtobin321@gmail.com</Dropdown.Item>
                             <Dropdown.Item href="https://github.com/jtobin321" target="_blank">Github</Dropdown.Item>
                             <Dropdown.Item href="https://www.linkedin.com/in/jack-tobin/" target="_blank">LinkedIn</Dropdown.Item>
+                            <Dropdown.Item href="https://twitter.com/__jack_tobin__" target="_blank">Twitter</Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>
                 </Container>
--- src/views/Home.js
+++ src/views/Home.js
@@ -3,29 +3,150 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 
-import logo from '../logos/waving_hand_emoji.png';
+import emoji from '../logos/waving_hand_emoji.png';
+import logo from '../logos/full-logo.png';
 
 import {
     Container,
+    Visibility,
+    Icon,
+    Menu,
+    Segment,
     Header,
-    Image,
-  } from 'semantic-ui-react'
+    Grid,
+    List,
+    Divider,
+    Image
+} from 'semantic-ui-react'
+
+const menuStyle = {
+    border: 'none',
+    borderRadius: 0,
+    boxShadow: 'none',
+    marginBottom: '1em',
+    marginTop: '4em',
+    transition: 'box-shadow 0.5s ease, padding 0.5s ease',
+  }
+  
+  const fixedMenuStyle = {
+    backgroundColor: '#fff',
+    border: '1px solid #ddd',
+    boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
+  }
+  
+  const overlayStyle = {
+    float: 'left',
+    margin: '0em 3em 1em 0em',
+  }
+  
+  const fixedOverlayStyle = {
+    ...overlayStyle,
+    position: 'fixed',
+    top: '80px',
+    zIndex: 10,
+  }
+  
+  const overlayMenuStyle = {
+    position: 'relative',
+    left: 0,
+    transition: 'left 0.5s ease',
+  }
+  
+  const fixedOverlayMenuStyle = {
+    ...overlayMenuStyle,
+    left: '800px',
+  }
+  
+  const LeftImage = () => (
+    <Image
+      floated='left'
+      size='medium'
+      src='/images/wireframe/square-image.png'
+      style={{ margin: '2em 2em 2em -4em' }}
+    />
+  )
+  
+  const RightImage = () => (
+    <Image
+      floated='right'
+      size='medium'
+      src='/images/wireframe/square-image.png'
+      style={{ margin: '2em -4em 2em 2em' }}
+    />
+  )
+  
+  const Paragraph = () => (
+    <p>
+      {[
+        'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ',
+        'tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas ',
+        'semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ',
+        'ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean ',
+        'fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. ',
+        'Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor ',
+        'neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, ',
+        'accumsan porttitor, facilisis luctus, metus',
+      ].join('')}
+    </p>
+  )
 
 const Home = (props) => {
     return (
         <div>
             <NavBar />
-            <Container text style={{ marginTop: '7em' }}>
-                <Header as='h1'>Jack Tobin - Software Engineer</Header>
-                <div style={{fontSize: "20px"}}>
-                <p>
-                    Hi There<Image size='mini' src={logo} floated="left" />
-                </p>
-                
-                <p>I'm glad that you have decided to visit, quite possibly, one of the best portfolios that you will ever look at.</p>
-                </div>
-            </Container>
-                    <Footer />
+<Container text style={{marginTop: "5em"}}>
+<Header centered>Jack Tobin - Personal Portfolio</Header>
+          
+            <Paragraph />
+            <Paragraph />
+          <LeftImage />
+
+          <Paragraph />
+          <RightImage />
+          <Paragraph />
+          <LeftImage />
+
+          <Paragraph />
+          <RightImage />
+          <Paragraph />
+        </Container>
+
+            <Segment inverted style={{ margin: '5em 0em 0em', padding: '5em 0em' }} vertical>
+                <Container textAlign='center'>
+                    <Grid columns={4} divided stackable inverted>
+                        <Grid.Row>
+                            <Grid.Column />
+                            <Grid.Column>
+                                <Header inverted as='h4' content='Jack Tobin - Portfolio' />
+                                <p>
+                                    This portfolio web application was created with the React Javascript Library
+                                    and styled with the React integration for Semantic UI.
+                                    All rights reserved.
+                  </p>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Header inverted as='h4' content='Links' />
+                                <List link inverted>
+                                    <List.Item as='a' href="https://github.com/jtobin321" target="_blank">Github</List.Item>
+                                    <List.Item as='a' href="https://linkedin.com/in/jack-tobin" target="_blank">LinkedIn</List.Item>
+                                    <List.Item as='a' href="https://twitter.com/__jack_tobin__" target="_blank">Twitter</List.Item>
+                                </List>
+                            </Grid.Column>
+                            <Grid.Column />
+                        </Grid.Row>
+                    </Grid>
+                    <Divider inverted section />
+                    <Image src={logo} centered size='small' />
+                    <List horizontal inverted divided link size='small'>
+                        <List.Item>
+                            Created by Jack Tobin
+              </List.Item>
+                        <List.Item as='a' href='#'>
+                            Contact Me
+              </List.Item>
+                    </List>
+                </Container>
+            </Segment>
         </div>
     );
 }
